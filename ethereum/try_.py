968e8cc8adcdd064d6297b7046e312134736f271
xloem <0xloem@gmail.com>
2021-07-10 14:21:03 +0000

    -

--- ethereum/try_.py
+++ ethereum/try_.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from web3 import Web3
-from web3.auto import w3 as w3_local
+from web3 import Web3, _utils as utils
+#from web3.auto import w3 as w3_local
 w3_cloud = Web3(Web3.HTTPProvider('https://cloudflare-eth.com'))
 try:
-    if w3_local.isConnected() and not w3_local.eth.is_syncing():
-        w3 = w3_local
-    else:
+    #if w3_local.isConnected() and not w3_local.eth.is_syncing():
+    #    w3 = w3_local
+    #else:
         raise Exception()
 except:
     w3 = w3_cloud
@@ -20,6 +20,15 @@ while True:
     print('not connected yet, waiting...')
     import time
     time.sleep(0.5)
+
+import abi
+
+import sqlite3
+db = sqlite3.connect('dex.sqlite')
+with db as c:
+    c.execute('CREATE TABLE IF NOT EXISTS tokens (addr BLOB PRIMARY KEY, symbol TEXT)') 
+    c.execute('CREATE TABLE IF NOT EXISTS dex (addr BLOB PRIMARY KEY)') 
+    c.execute('INSERT OR IGNORE INTO dex VALUES(?)', [utils.encoding.to_bytes(hexstr=abi.uniswapv2_factory_addr)])
     
 
 tokenaddrs = {
@@ -29,8 +38,6 @@ tokenaddrs = {
 accaddrs = {
     'Uniswap V2: Dai 2': '0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11'
 }
-
-import abi
 
 # filters can be polled synchronously or asynchronously, see web3py docs
 # filters only work on local nodes
@@ -42,13 +49,21 @@ import abi
 # getLogs works on remote and local nodes, but appears to need a contract address
 
 uniswapv2ct = w3.eth.contract(address=abi.uniswapv2_factory_addr,abi=abi.uniswapv2_factory)
-pairs = uniswapv2ct.events.PairCreated.getLogs(fromBlock=w3.eth.block_number-127,toBlock='latest')
-print(len(pairs),'recent new pairs')
-for pair in pairs:
-    token0 = w3.eth.contract(address=pair.args.token0, abi=abi.erc20)
-    token1 = w3.eth.contract(address=pair.args.token1, abi=abi.erc20)
-    print(f'{token0.functions.symbol().call()}-{token1.functions.symbol().call()}')
+
+# i strangely get a mismatched abi error unless i include this line
+testrecpt = w3.eth.getTransactionReceipt('0xb31fcb852b18303c672b81d90117220624232801ad949bd4047e009eaed73403')
+#uniswapv2ct.events.PairCreated()#.processLog(testrecpt.logs[0])
+
+with db as c:
+    #lasttailblock = w3.eth.block_number
+    #for tailblock in range(lasttailblock, 0, -127):
+    for pair in uniswapv2ct.events.PairCreated().getLogs(fromBlock=w3.eth.block_number-127):#tailblock-127,toBlock=tailblock):
+        for tokenaddr in (pair.args.token0, pair.args.token1):
+            token = w3.eth.contract(address=tokenaddr, abi=abi.erc20)
+            symbol = token.functions.symbol().call()
+            print(symbol)
+            c.execute('INSERT OR IGNORE INTO tokens VALUES(?, ?)', (utils.encoding.to_bytes(hexstr=tokenaddr), symbol))
 
 daicontract = w3.eth.contract(address=tokenaddrs['DAI'],abi=abi.erc20)
-somelogs = daicontract.events.Transfer.getLogs(fromBlock=daicontract.web3.eth.block_number-127,toBlock='latest')
+somelogs = daicontract.events.Transfer().getLogs(fromBlock=daicontract.web3.eth.block_number-127,toBlock='latest')
 
