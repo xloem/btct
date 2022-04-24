36598cf8dde0c49a08db1be588342062fdf541cc
Karl Semich <0xloem@gmail.com>
2022-04-24 11:38:55 -0400

    -

--- ethereum/test_sol.py
+++ ethereum/test_sol.py
@@ -9,6 +9,7 @@ if not os.path.exists('solana.seed'):
 else:
     with open('solana.seed', 'rb') as keyfile:
         key = solana.keypair.Keypair.from_seed(keyfile.read())
+print('main account:', key.public_key)
 print('starting ..')
 
 async def main():
@@ -16,13 +17,14 @@ async def main():
     for pair in dex.pairs():
         print(pair.db, pair.db.token0.symbol, pair.db.token1.symbol)
         if 'SOL' in pair.db.token0.symbol:
+            print('market = ', pair.db.addr)
             base_acct = pair.token0.account(key)
-            quote_acct = pair.token1.account(key)
             base_balance = pair.token0.balance(base_acct)
+            print(pair.db.token0.symbol, str(pair.db.token0.addr), 'acct =', base_acct, 'balance =', base_balance)
+            quote_acct = pair.token1.account(key)
             quote_balance = pair.token1.balance(quote_acct)
-            print(base_balance, quote_balance)
+            print(pair.db.token1.symbol, str(pair.db.token1.addr), 'acct =', quote_acct, 'balance =', quote_balance)
 
-            print(pair.db.addr)
             await pair.pump()
 
 asyncio.run(main())
